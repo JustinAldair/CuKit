43af965c0a10a09fe048a95a60ccb4553ae123c4
jonaraider4@hotmail.com <jonaraider4@hotmail.com>
2023-03-19 17:04:45 -0600

    Interfaz del registro

--- cukit/app/src/main/java/com/example/cukit/Registrate.java
+++ cukit/app/src/main/java/com/example/cukit/Registrate.java
@@ -2,13 +2,28 @@ package com.example.cukit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Registrate extends AppCompatActivity {
+    private TextView tv_iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrate);
+
+        //Volver al inicio de sesion
+        tv_iniciarSesion = findViewById(R.id.r_tv_iniciarSesion);
+        tv_iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), inicioSesion.class);
+                startActivity(intent);
+            }
+        });
     }
 }
