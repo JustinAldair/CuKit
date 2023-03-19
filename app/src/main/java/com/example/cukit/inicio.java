170c156ccc184954fe5b8de44c7a44510bb2fa8e
Rafaelmag3 <rafaelmonrroy045gmail.com>
2023-03-18 21:30:44 -0600

    Inicio de recetas

--- app/src/main/java/com/example/cukit/inicio.java
+++ app/src/main/java/com/example/cukit/inicio.java
@@ -2,7 +2,16 @@ package com.example.cukit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class inicio extends AppCompatActivity {
 
@@ -10,5 +19,8 @@ public class inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+
+
     }
 }
