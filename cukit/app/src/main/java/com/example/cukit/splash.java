9c6c157f22e1b372e9e6ba418c788b425e197618
EfrenOr <efren.or@outlook.com>
2023-04-12 22:20:02 -0600

    Actualización

--- cukit/app/src/main/java/com/example/cukit/splash.java
+++ cukit/app/src/main/java/com/example/cukit/splash.java
@@ -1,11 +1,16 @@
 package com.example.cukit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 public class splash extends AppCompatActivity {
 
@@ -16,22 +21,49 @@ public class splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //SharedPreferences es una clase para alamacenar información y preferencias en la aplicación
-        SharedPreferences localStorage = getSharedPreferences("localstorage", MODE_PRIVATE);
-        //Alamaceno la IP del servidor para usarlo en futuras peticiones y cualquier cambio en la ip unicamente
-        //modifcar la linea 25 con la nueva ip
-        SharedPreferences.Editor editor = localStorage.edit();
-        editor.putString("ip", "192.168.1.78");
-        editor.commit();
+        // Creamos un nuevo AlertDialog.Builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        // Creamos un handler para lanzar la actividad principal luego del tiempo de duración del splash screen
-        new Handler().postDelayed(new Runnable() {
+// Configuramos el diálogo
+        builder.setTitle("Ingresar texto");
+        builder.setMessage("Por favor, ingrese el texto que desea:");
+
+        // Creamos el layout personalizado para el diálogo
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.input, null);
+        builder.setView(view);
+
+        // Agregamos el botón de confirmación
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
             @Override
-            public void run() {
-                Intent mainIntent = new Intent(splash.this, inicioSesion.class);
-                startActivity(mainIntent);
-                finish(); // Cerramos la actividad del splash screen
+            public void onClick(DialogInterface dialog, int which) {
+                // Obtenemos el texto ingresado por el usuario
+                EditText editText = view.findViewById(R.id.et_ip);
+                String ip = editText.getText().toString();
+
+                //SharedPreferences es una clase para alamacenar información y preferencias en la aplicación
+                SharedPreferences localStorage = getSharedPreferences("localstorage", MODE_PRIVATE);
+                //Alamaceno la IP del servidor para usarlo en futuras peticiones y cualquier cambio en la ip unicamente
+                //modifcar la linea 25 con la nueva ip
+                SharedPreferences.Editor editor = localStorage.edit();
+                editor.putString("ip", ip);
+                editor.commit();
+
+                // Creamos un handler para lanzar la actividad principal luego del tiempo de duración del splash screen
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent(splash.this, inicioSesion.class);
+                        startActivity(mainIntent);
+                        finish(); // Cerramos la actividad del splash screen
+                    }
+                }, dura);
+
             }
-        }, dura);
+        });
+
+        // Mostramos el diálogo
+        builder.show();
+
     }
 }
